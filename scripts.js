0a0299609545da51dc59fec75a9b973ce98f350e
Philippe Henrique Ø <philippehenrique@outlook.com>
2017-10-12 09:39:21 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,43 +1,21 @@
-
 $(document).ready(function() {
 
-	currentPage = 'bot1';
-	atualPage();
-
 	// Mostra a página inicial
-	$('.page.bot1').fadeIn(1000);
+	$('.page0').fadeIn(1000);
 
-	// Ao clicar no elemento com classe 'action', executa a função (menuSelect)
-	$('.action').click(menuSelect);
+	$('.action').each(function(index, el) {
+		$(el).click(function() {
+			$('.action').removeClass('active');
+			$('.page').slideUp();
+			$('.page'+ index).slideDown();
+			$(el).addClass('active');
+		});
+	});
+
+	$('.action.bot').each(function(index, el) {
+		$('#main').append('<div class="page page' + (index + 1) + ' bot"></div>');
+	});
+
+	$('.page.bot').load('content.html');
 
 });
-
-
-function menuSelect() {
-
-	if ($(this).hasClass('home')) {
-
-		$('.page').slideUp();
-		$('.page.home').slideDown();
-		currentPage = 'home';
-		atualPage();
-	}
-
-	else if ($(this).hasClass('bot1')) {
-
-		$('.page').slideUp();
-		$('.page.bot1').slideDown();
-		currentPage = 'bot1';
-		atualPage();
-	}
-
-	else { alert("Este item não possuí uma classe para o redirecionamento atribuída"); }
-}
-
-function atualPage() {
-
-	if ($('.action').hasClass(currentPage)) {
-		$('.action').removeClass('active');
-		$('.'+ currentPage).addClass('active');
-	}
-}
